5b166242dd231bbbc25bd5de92b0314a521791ac
Joseph-Dylan <jbrisenom2300@alumno.ipn.mx>
2025-10-11 17:12:16 -0600

    descripción de ejercicios

--- 06Algoritmia/JS/algoritmia.js
+++ 06Algoritmia/JS/algoritmia.js
@@ -13,32 +13,30 @@ function problema1(){
 }
 
 function problema2(){
-    // Leer los valores de los inputs
-const x1 = parseFloat(document.getElementById("p2-x1").value);
-const x2 = parseFloat(document.getElementById("p2-x2").value);
-const x3 = parseFloat(document.getElementById("p2-x3").value);
-const x4 = parseFloat(document.getElementById("p2-x4").value);
-const x5 = parseFloat(document.getElementById("p2-x5").value);
+    const x1 = parseFloat(document.getElementById("p2-x1").value);
+    const x2 = parseFloat(document.getElementById("p2-x2").value);
+    const x3 = parseFloat(document.getElementById("p2-x3").value);
+    const x4 = parseFloat(document.getElementById("p2-x4").value);
+    const x5 = parseFloat(document.getElementById("p2-x5").value);
 
-const y1 = parseFloat(document.getElementById("p2-y1").value);
-const y2 = parseFloat(document.getElementById("p2-y2").value);
-const y3 = parseFloat(document.getElementById("p2-y3").value);
-const y4 = parseFloat(document.getElementById("p2-y4").value);
-const y5 = parseFloat(document.getElementById("p2-y5").value);
+    const y1 = parseFloat(document.getElementById("p2-y1").value);
+    const y2 = parseFloat(document.getElementById("p2-y2").value);
+    const y3 = parseFloat(document.getElementById("p2-y3").value);
+    const y4 = parseFloat(document.getElementById("p2-y4").value);
+    const y5 = parseFloat(document.getElementById("p2-y5").value);
 
-const xs = [x1, x2, x3, x4, x5];
-const ys = [y1, y2, y3, y4, y5];
+    const xs = [x1, x2, x3, x4, x5];
+    const ys = [y1, y2, y3, y4, y5];
 
-xs.sort((a,b) => a-b);
-ys.sort((a,b) => b-a);
+    xs.sort((a,b) => a-b);
+    ys.sort((a,b) => b-a);
 
-let productoEscalar = 0;
-for(let i = 0; i < 5; i++){
-    productoEscalar += xs[i] * ys[i];
-}
+    let productoEscalar = 0;
+    for(let i = 0; i < 5; i++){
+        productoEscalar += xs[i] * ys[i];
+    }
 
-document.getElementById("p2-output").textContent = "Producto escalar mínimo: " + productoEscalar;
-
+    document.getElementById("p2-output").textContent = "Producto escalar mínimo: " + productoEscalar;
 }
 
 function problema3(){
